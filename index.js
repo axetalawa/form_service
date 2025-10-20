68944eaee3e6fe1415d1237e2fa789797e725fb7
axetalawa <axetalawa@gmail.com>
2025-10-20 15:13:06 -0400

    update

--- index.js
+++ index.js
@@ -46,4 +46,6 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`⚙️ Ritual Form Service running on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`⚙️ Ritual Form Service running on port ${PORT}`)
+);
